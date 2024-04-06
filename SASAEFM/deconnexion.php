900537872a619dea6f12edb88dc8fefbc7774a66
rorodughetto <romain.eyquem@esme.fr>
2024-04-06 16:15:04 +0200

    cleanup

--- SASAEFM/deconnexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Détruisez la session actuelle.
-session_destroy();
-
-// Redirigez l'utilisateur vers la page de connexion ou une autre page appropriée.
-header('Location: index.php');
-exit;
